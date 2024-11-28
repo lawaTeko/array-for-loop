b0343c6a3e1e873853c35d487f8c32dbd117595d
unknown <lawateko@gmail.com>
2024-11-28 23:25:55 +0700

    commit

---
+++ array-loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// 							0		1		2		  3			4
+	string buahArray[5] = {"Coklat", "Jeruk", "Pepaya", "Pisang", "Mangga"};
+	int nilaiArray[5] = {10, 20, 30, 40, 50};
+	
+	for (string namaBuah : buahArray){
+		buahArray[2] = "Wortel"; 
+		cout << namaBuah << " \n";
+	}
+	
+	cout << endl;
+	
+	for (int i : nilaiArray){
+		cout << i << " \n";
+	}
+	
+	return 0;
+}
